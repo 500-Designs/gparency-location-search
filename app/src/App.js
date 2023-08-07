725fbdb4e31353f152e81096677ca01294e981d1
jdev <jlacandula@gmail.com>
2023-08-08 07:48:28 +0800

    chore: cleanup comments

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,6 @@
 import './App.css';
-// import LocationSearch from './components/LocationSearch';
-// import MapContainer from './components/MapContainer';
-// import AutoComplete from './components/AutoComplete';
 import SearchSuggestions from './components/SearchSuggestions';
 
-// import Autocomplete from "react-google-autocomplete";
-// import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
-
-// const GOOGLE_PLACES_API_KEY = 'AIzaSyCl-7pJgI-AXJmpjtYmrJvKtL7p6bP4_W0';
 
 function App() {
   return (
